f3dcf90d111310e56ee67cf504ebb515a518e3e6
Adam <adambh@outlook.com>
2021-10-11 10:54:02 +0300

    Fix sometimes it doesn't catch the merge button

--- spr-fix-github.user.js
+++ spr-fix-github.user.js
@@ -6,33 +6,35 @@
 // @include     *://github.com/*
 // @version     1
 // @grant       none
-// @run-at      document-start
 // ==/UserScript==
 
-function doStuff() {
-    // PR Merge button
-    checkElementThenRun('.merge-message'); 
+(function() {
+    function main() {
+        // PR Merge button
+        checkElementThenRun('.merge-message'); 
 
-    // New PR button
-    checkElementThenRun("#repo-content-pjax-container > div > div.d-flex.flex-justify-between.mb-md-3.flex-column-reverse.flex-md-row.flex-items-end > div.ml-3.d-flex.flex-justify-between.width-full.width-md-auto");
-}
-
-const work = function(selector) {
-    let el = document.querySelector(selector);
-    if (typeof el !== 'undefined') {
-        el.remove();
+        // New PR button
+        checkElementThenRun("#repo-content-pjax-container > div > div.d-flex.flex-justify-between.mb-md-3.flex-column-reverse.flex-md-row.flex-items-end > div.ml-3.d-flex.flex-justify-between.width-full.width-md-auto");
     }
-};
 
-const checkElementThenRun = function(selector) {
-    if (document.querySelector(selector) == null && window.requestAnimationFrame != undefined) {
-        window.requestAnimationFrame(function(){ checkElementThenRun(selector, function() { work(selector); }); });
-    } else {
-        work(selector);
+    const work = function(selector) {
+        let el = document.querySelector(selector);
+        if (typeof el !== 'undefined') {
+            el.remove();
+        }
+    };
+
+    const checkElementThenRun = function(selector) {
+        if (document.querySelector(selector) == null && window.requestAnimationFrame != undefined) {
+            window.requestAnimationFrame(function() { checkElementThenRun(selector, function() { work(selector); }); });
+        } else {
+            work(selector);
+        }
     }
-}
 
-// Hook mouse clicks instead of looping
-document.body.addEventListener('click', doStuff, true);
+    // Hook mouse clicks instead of looping
+    document.body.addEventListener('click', main, true);
 
-doStuff();
+    // Entrypoint
+    setTimeout(main, 500);
+})();
